9fcf7b21a2341c5fbb0d4dd5d5b3b65c56467038
anil kumar tudu <anil.3brain@gmail.com>
2019-08-29 15:45:45 +0530

    initial folder structure

---
+++ src/booksApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const booksApp = () => (
+  <View>
+    <Text>Books App</Text>
+  </View>
+);
+export default booksApp;
---
+++ src/theme/index.js
@@ -0,0 +1,6 @@
+export default Theme = {
+  colors: {},
+  fontFamily: {},
+  fontSize: {},
+  fontWeight: {},
+};
