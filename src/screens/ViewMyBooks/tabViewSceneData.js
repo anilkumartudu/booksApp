10053f3517604cf6b2143a20ba5034675b2ec4bd
anil kumar tudu <anil.3brain@gmail.com>
2019-09-12 18:43:05 +0530

    created horizontal list component

--- src/screens/ViewMyBooks/tabViewSceneData.js
+++ src/screens/ViewMyBooks/tabViewSceneData.js
@@ -1,34 +1,67 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {HorizontalList} from '../../components';
+import {MainContainer} from './style';
 
 export const currentlyReadingRoute = () => {
+  let data = [{}];
+
   return (
-    <View>
-      <Text>currentlyReading</Text>
-    </View>
+    <MainContainer>
+      <HorizontalList data={data} />
+    </MainContainer>
   );
 };
 
 export const didNotFinishedRoute = () => {
+  let data = [
+    {
+      name: 'Book 4: Harry Potter and the Goblet of Fire',
+    },
+  ];
+
   return (
-    <View>
-      <Text>didNotFinished</Text>
-    </View>
+    <MainContainer>
+      <HorizontalList data={data} />
+    </MainContainer>
   );
 };
 
 export const readRoute = () => {
+  let data = [
+    {
+      name: "Book 1: Harry Potter and the Sorcerer's Stone",
+    },
+    {
+      name: 'Book 2: Harry Potter and the Chamber of Secrets',
+    },
+    {
+      name: 'Book 3: Harry Potter and the Prisoner of Azkaban',
+    },
+  ];
+
   return (
-    <View>
-      <Text>read</Text>
-    </View>
+    <MainContainer>
+      <HorizontalList data={data} />
+    </MainContainer>
   );
 };
 
 export const wantToReadRoute = () => {
+  let data = [
+    {
+      name: 'Book 5: Harry Potter and the Order of the Phoenix',
+    },
+    {
+      name: 'Book 6: Harry Potter and the Half-Blood Prince',
+    },
+    {
+      name: 'Book 7: Harry Potter and the Deathly Hallows',
+    },
+  ];
+
   return (
-    <View>
-      <Text>wantToRead</Text>
-    </View>
+    <MainContainer>
+      <HorizontalList data={data} />
+    </MainContainer>
   );
 };
